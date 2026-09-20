25dcb9ac4f08e753144fab2d85aebf499dc1e315
aelkhoreiby <a.elkhoreiby@gmail.com>
2026-09-20 12:01:53 +0300

    Polish Terms page and use shared footer

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -46,19 +46,6 @@ export default function TermsPage() {
           استمرار استخدام الموقع بعد هذه التغييرات يعني استمرار قبول الشروط المحدثة.
         </p>
       </article>
-
-      <Footer />
     </main>
-  );
-}
-
-function Footer() {
-  return (
-    <footer className="site-footer">
-      <span>Business &amp; Money Tools</span>
-      <span>
-        <Link href="/privacy/">الخصوصية</Link> · <Link href="/terms/">الشروط</Link> · <Link href="/about/">عن الموقع</Link>
-      </span>
-    </footer>
   );
 }
